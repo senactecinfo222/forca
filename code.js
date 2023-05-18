2933ceba2b030acb45533ce552ddeb76c19b0db8
FABIO DE LIMA <fabio.lima@senacsp.edu.br>
2023-05-18 14:37:25 -0300

    sorteio

--- code.js
+++ code.js
@@ -7,12 +7,16 @@ var words = ['desoxirribonucleico','beija flor','abacate',
 'ficus','riboflavina','bigorna', 'planador','acelerador',
 'cinema','clarevidencia','bico de busen', 'tartarugas ninjas']
 
+//var words = ['tartarugas ninjas']
+
 var letters = keys.length
 var contwords = words.length
 var drawnword
 var wordtype
 var wordcount
 var word = []
+var letter
+var quantletras = 0
 
 for (var i = 0; i < letters; i++) {
     var divb = document.createElement('div')
@@ -21,6 +25,8 @@ for (var i = 0; i < letters; i++) {
     divb.setAttribute('id', i)
     document.getElementById(i).appendChild(button)
     button.setAttribute('val', keys[i])
+    button.setAttribute('id',  keys[i])
+    button.setAttribute('onclick','getLetter(this.id)')
     button.innerHTML = keys[i]
     //----------------------formatacao---------------
     divb.setAttribute('class', 'col')
@@ -68,7 +74,34 @@ for(var i=0; i < word.length; i++){
         var input = document.createElement('input')
         input.setAttribute('type','text')
         input.setAttribute('class', 'form-control')
+        input.setAttribute('value', word[i][j])
+        input.setAttribute('id', quantletras)
+        input.style.color = 'white'
         document.getElementById('word').appendChild(input)
+        quantletras = quantletras + 1
    }
+    var espaco = document.createElement('input')
+    espaco.setAttribute('type','text')
+    espaco.setAttribute('class', 'form-control')
+    espaco.setAttribute('value', " ")
+    espaco.style.border.color = 'white'
+    document.getElementById('word').appendChild(espaco)
 }
 
+function getLetter(id){
+    letter = id
+    verifica()
+}
+
+function verifica(){
+    var acertos = 0
+    for(var i=0; i < quantletras; i++){
+        if(letter == document.getElementById(i).value){
+            document.getElementById(i).style.color = 'black'
+            acertos = acertos + 1
+        }
+    }
+    if(acertos == 0){
+        document.getElementById("stem").style.visibility = 'visible'
+    }
+}
